441cdd09e1680011ee37f7643345976bc977a713
Kevin <changk2@oregonstate.edu>
2025-04-05 14:37:54 -0700

    react-konva kinda cooking

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { Stage, Layer, Rect, Circle } from 'react-konva';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+function Canvas() {
+  return (
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Rect x={20} y={20} width={100} height={100} fill="blue" draggable />
+        <Circle x={150} y={150} radius={50} fill="red" draggable />
+      </Layer>
+    </Stage>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Canvas></Canvas>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
