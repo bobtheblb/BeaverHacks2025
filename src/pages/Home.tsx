b3e058b0ff3b5220e9e6e98fa4cc8732d17e991e
Jonathan Pham <phamjonathan364@outlook.com>
2025-04-05 15:12:27 -0700

    Added routes

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router';
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-center px-4">
+      <h1 className="text-4xl md:text-6xl font-extrabold mb-6 font-borel">
+        deus.ai
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
