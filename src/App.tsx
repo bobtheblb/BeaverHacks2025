81ee95c9f55b91f3a1f553e269c3da4a79774d8e
Kevin <changk2@oregonstate.edu>
2025-04-05 15:23:34 -0700

    finished single octave of piano

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import { useEffect, useState } from 'react'
-import { Stage, Layer, Rect, Circle } from 'react-konva';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-function Canvas() {
-  return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer>
-        <Rect x={20} y={20} width={100} height={100} fill="blue" draggable />
-        <Circle x={150} y={150} radius={50} fill="red" draggable />
-      </Layer>
-    </Stage>
-  );
-}
+import { useEffect, useState } from 'react'
+import Keyboard from './Keyboard.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Canvas></Canvas>
+      <Keyboard/>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
