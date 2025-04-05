5857cadbbe18fd5d2a37417d7faf024c53112e36
audreyau <auau@oregonstate.edu>
2025-04-05 15:08:45 -0700

    Integrate Tone.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import * as Tone from 'tone'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [started, setStarted] = useState(false)
+  const middleC = 'C4'
+  const noteLength = '8n'
 
+  const handleKeyDown = async (event) => {
+    if (event.key === ' ') {
+      if (!started) {
+        await Tone.start()
+        setStarted(true)
+        console.log('Audio context started')
+      }
+
+      const synth = new Tone.PolySynth().toDestination()
+      synth.triggerAttackRelease(middleC, noteLength)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown)
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [started])
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Deus</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p>press space</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
